f28dacfa80a8c6842a402b866e945cb730a00fab
DrX7FFF <dubourg.v@gmail.com>
2024-04-22 00:03:52 +0200

    save

--- test.py
+++ test.py
@@ -2,14 +2,17 @@ import json
 import hid
 
 for x in hid.enumerate(0x046D):
-    print(
-        "{:04x}  {}  at: {} {}".format(
-            x["product_id"],
-            x["product_string"] or "(None)",
-            x['usage'],
-            x['usage_page'],
-            # x["path"],
-        )
+    if x["product_id"] == 0xc52b and x['usage'] == 0x0001 and x['usage_page'] == 0xFF00:
+        print("Bolt")
+    if x['usage'] == 0x0202 and x['usage_page'] == 0xff43:
+    	print(
+			"Bluetooth {} ({:04x}) = {}".format(
+				x["product_string"] or "(None)",
+				x["product_id"],
+				# x['usage'],
+				# x['usage_page'],
+				x["path"],
+			)
     )
 
 # for d in hid.enumerate():
